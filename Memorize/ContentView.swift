ff9d65455eb786762c00ee82981271f2502ef25a
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-04-12 15:28:14 +0100

    memorizgame

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4){indx in
+                
+                cardsviw()
+            }
+            
+           
+        }
+        .padding()
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        .font(Font.largeTitle)
+        
+        
+        
+       
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct cardsviw: View {
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 10.0).fill()
+           // RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("🇩🇿")
+                .padding()
+            
+        }
+       
     }
 }
