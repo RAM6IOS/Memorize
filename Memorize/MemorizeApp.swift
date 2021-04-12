d4ec78e4d7c2fa1f27fa38cec181b99f8aecbf0f
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-04-12 12:11:48 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by MAC on 12/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
